95a5dcef74ce0a507da25f49adf02d7506b7a02a
ohyusheng <humbleoh@gmail.com>
2026-04-20 18:55:48 +0800

    refactor(delta_queue): apply double pointer trick

--- src/container/intrusive/delta_queue.cpp
+++ src/container/intrusive/delta_queue.cpp
@@ -14,66 +14,36 @@ std::size_t delta_queue::size() const noexcept
 
 void delta_queue::insert(key_type abs_key, node_type& entry)
 {
-    if (!head_) {
-        entry.delta = abs_key;
-        entry.next = nullptr;
-        head_ = &entry;
-    } else if (abs_key < head_->delta) {
-        entry.delta = abs_key;
-        entry.next = head_;
-        head_->delta -= abs_key;
-        head_ = &entry;
-    } else {
-        node_type* cur = head_;
-        node_type* prev = nullptr;
-        delta_type accum = 0;
-        while (cur && cur->delta + accum <= abs_key) {
-            accum += cur->delta;
-            prev = cur;
-            cur = cur->next;
-        }
-
-        entry.delta = abs_key - accum;
-        entry.next = cur;
-        prev->next = &entry;
-        if (cur) {
-            cur->delta -= entry.delta;
-        }
+    delta_type accum = 0;
+    node_type** indirect = &head_;
+    while (*indirect && (*indirect)->delta + accum <= abs_key) {
+        accum += (*indirect)->delta;
+        indirect = &(*indirect)->next;
     }
 
+    entry.delta = abs_key - accum;
+    if (*indirect) {
+        (*indirect)->delta -= entry.delta;
+    }
+    entry.next = *indirect;
+    *indirect = &entry;
     ++size_;
 }
 
 bool delta_queue::erase(node_type& entry) noexcept
 {
-    if (!head_) {
-        return false;
-    }
-
-    if (head_ == &entry) {
-        if (entry.next) {
-            entry.next->delta += entry.delta;
-        }
-        head_ = entry.next;
-        entry.next = nullptr;
-        --size_;
-        return true;
-    }
-
-    node_type* prev = head_;
-    node_type* cur = head_->next;
-    while (cur) {
-        if (cur == &entry) {
+    node_type** indirect = &head_;
+    while (*indirect) {
+        if (*indirect == &entry) {
             if (entry.next) {
                 entry.next->delta += entry.delta;
             }
-            prev->next = entry.next;
+            *indirect = entry.next;
             entry.next = nullptr;
             --size_;
             return true;
         }
-        prev = cur;
-        cur = cur->next;
+        indirect = &(*indirect)->next;
     }
 
     return false;
