fdcc5048dea551604ab27ad6d7c6cf5ebc0a31d2
ohyusheng <humbleoh@gmail.com>
2026-04-20 18:33:32 +0800

    feat(delta_queue): add `erase` method

--- include/riz/container/intrusive/delta_queue.h
+++ include/riz/container/intrusive/delta_queue.h
@@ -12,7 +12,7 @@ class delta_queue : public noncopyable {
 public:
     using delta_type = std::uint32_t;
     using key_type = delta_type;
-    struct node {
+    using node_type = struct node {
         delta_type delta;
         node* next;
     };
@@ -21,16 +21,17 @@ public:
     bool empty() const noexcept;
     std::size_t size() const noexcept;
 
-    void insert(key_type abs_key, node& entry);
-    node* pop_front() noexcept;
+    void insert(key_type abs_key, node_type& entry);
+    bool erase(node_type& node) noexcept;
+    node_type* pop_front() noexcept;
 
     template<typename Callback>
-        requires std::invocable<Callback, node*>
+        requires std::invocable<Callback, node_type*>
     void advance(delta_type elapsed, Callback&& callback)
     {
         while (head_ && head_->delta <= elapsed) {
             elapsed -= head_->delta;
-            node* entry = pop_front();
+            node_type* entry = pop_front();
             callback(entry);
         }
         if (head_ && elapsed > 0) {
@@ -39,7 +40,7 @@ public:
     }
 
 private:
-    node* head_ {nullptr};
+    node_type* head_ {nullptr};
     std::size_t size_ {0u};
 };
 
