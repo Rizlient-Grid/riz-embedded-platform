b6aaee8468663cd4e6ed5b60033318b4573c6cbd
ohyusheng <humbleoh@gmail.com>
2026-04-24 02:03:21 +0800

    feat: add `singleton` base class
    Key changes:
    
    - Add compile-time public ctor check
    - Enforce `singleton` to be `immovable`

--- include/riz/constraints.h
+++ include/riz/constraints.h
@@ -8,6 +8,14 @@ struct noncopyable {
     noncopyable& operator=(const noncopyable&) = delete;
 };
 
+struct nonmovable {
+    nonmovable() = default;
+    nonmovable(const nonmovable&) = default;
+    nonmovable(nonmovable&&) = delete;
+    nonmovable& operator=(const nonmovable&) = default;
+    nonmovable& operator=(nonmovable&&) = delete;
+};
+
 struct moveonly {
     moveonly() = default;
     moveonly(moveonly&&) = default;
@@ -15,5 +23,16 @@ struct moveonly {
     moveonly& operator=(moveonly&&) = default;
     moveonly& operator=(const moveonly&) = delete;
 };
+
+struct immovable : noncopyable, nonmovable {};
+
+template<typename T>
+concept accessible_default_constructible = requires { T {}; };
+
+template<typename T>
+consteval bool has_public_default_constructible()
+{
+    return accessible_default_constructible<T>;
+}
 
 } // namespace riz
---
+++ include/riz/pattern/singleton.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <riz/constraints.h>
+
+#include <type_traits>
+
+namespace riz::pattern {
+
+template<typename T>
+class singleton : public immovable {
+public:
+    static T& instance()
+    {
+        static_assert(!riz::has_public_default_constructible<T>());
+        static T obj;
+        return obj;
+    }
+
+protected:
+    singleton() = default;
+};
+
+} // namespace riz::pattern
